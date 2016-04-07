9b3c6b21cc3e48653fe1fffb04cb0c0453fd7e21
dfefilov <dfefilov@wildapricot.com>
2016-04-07 20:15:46 +0300

    ДЗ: реализовал добавление комментария к статье

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,55 +1,58 @@
 import React, { Component, PropTypes } from 'react'
+import CommentAdd from './CommentAdd'
 import CommentList from './CommentList'
-// import { findDOMNode } from 'react-dom'
-// import toggleOpen from '../HOC/toggleOpen'
+import { findDOMNode } from 'react-dom'
 
 class Article extends Component {
-    render() {
-        const { article: { title }, selected } = this.props;
-        const style = selected ? { color: 'red' } : null;
 
+    render() {
+        const { article: { title }, isSelected, openItem, deleteArticle } = this.props
+        const style = isSelected ? {color: 'red'} : null
         return (
             <div ref = "articleContainer">
-                <h3 onClick = { this.handleClick } style = { style }>{ title }</h3>
-                <a href = "#" onClick = { this.handleSelect }>select this article</a>
+                <h3 onClick = {openItem} style = {style}>{title}</h3>
+                <a href = "#" onClick = {this.handleSelect}>select this article</a> | <a href = "#" onClick = {this.deleteArticle}>delete this article</a>
                 { this.getBody() }
             </div>
         )
     }
 
-    /*
+    addComment = (comment) => {
+        const { article: { id } } = this.props;
+
+        this.props.addComment(id, comment);
+    };
+
+    deleteArticle = (ev) => {
+        ev.preventDefault();
+
+        this.props.deleteArticle(this.props.article.id)
+    };
+
     componentDidMount() {
+/*
         console.log('---', this.refs);
         console.log('---', 'commentList: ', this.refs.commentList, findDOMNode(this.refs.commentList));
+*/
     }
-     */
-
-    getBody() {
-        const { article, expanded } = this.props;
-
-        return expanded ?
-            (
-                <section>
-                    { article.text }
-                    <CommentList comments = { article.comments } ref = "commentList" />
-                </section>
-            ) : null;
-    }
-
-    handleClick = (ev) => {
-        const { article: { id }, expandArticle } = this.props;
-
-        // this.props.toggleOpen();
-
-        expandArticle(id);
-    };
 
     handleSelect = (ev) => {
-        const { article: { id }, selectArticle } = this.props;
+        const { article: {id}, selectArticle } = this.props
+        selectArticle(id)
+    }
 
-        selectArticle(id);
-    };
+    getBody() {
+        if (!this.props.isOpen) return null
+        const { article } = this.props
+        return (
+            <section>
+                <p>{ article.text }</p>
+                <CommentAdd add = { this.addComment } />
+                <br/>
+                <CommentList comments = { article.getRelation('comments') } ref = "commentList" />
+            </section>
+        )
+    }
 }
 
-// export default toggleOpen(Article)
 export default Article
--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,50 +1,49 @@
 import React, { Component, PropTypes } from 'react'
-import Accordion from '../HOC/Accordion'
 import Article from './Article'
+import singleOpen from '../HOC/singleOpen'
 
 class AricleList extends Component {
-    state = { selectedArticles: [] };
+    state = {
+        selectedArticles: []
+    }
+
+    static propTypes = {
+        articles: PropTypes.array.isRequired,
+        deleteArticle: PropTypes.func.isRequired
+    }
 
     render() {
         return (
             <div>
-                <ul>{ this.getList() }</ul>
+                <ul>
+                    {this.getList()}
+                </ul>
             </div>
         )
     }
 
     getList() {
-        const { expandedItem } = this.props;
-
-        return this.props.articles.map((a) => {
-            return (
-                <li key={ a.id }>
-                    <Article
-                        article={ a }
-                        expandArticle={ this.expandArticle }
-                        expanded={ a.id == expandedItem }
-                        selectArticle={ this.selectArticle }
-                        selected={ this.state.selectedArticles.includes(a.id) }
-                    />
-                </li>
-            )
-        });
+        const { isOpen, openItem } = this.props
+        return this.props.articles.map((article, index) =>
+            <li key={article.id}>
+                <Article
+                    article = {article}
+                    openItem = {openItem(article.id)}
+                    isOpen = {isOpen(article.id)}
+                    addComment = { this.props.addComment }
+                    deleteArticle = {this.props.deleteArticle}
+                    isSelected = {this.state.selectedArticles.includes(article.id)}
+                    selectArticle = {this.selectArticle}
+                />
+            </li>
+        )
     }
-
-    expandArticle = (id) => {
-        const { expandItem } = this.props;
-
-        expandItem(id);
-    };
 
     selectArticle = (id) => {
         this.setState({
-            selectedArticles:
-                this.state.selectedArticles.includes(id)
-                    ? this.state.selectedArticles
-                    : this.state.selectedArticles.concat(id)
-        });
-    };
+            selectedArticles: this.state.selectedArticles.concat(id)
+        })
+    }
 }
 
-export default Accordion(AricleList)
+export default singleOpen(AricleList)
