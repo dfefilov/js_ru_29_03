5c461116e7997d96a7cc0aae34a98f6ed5a2006d
dfefilov <dfefilov@wildapricot.com>
2016-04-08 17:34:41 +0300

    Visual Studio Code test

--- src/fixtures.js
+++ src/fixtures.js
@@ -54,7 +54,7 @@ export const normalizedComments = [
 export const normalizedArticles = [
     {
         "id": "56c782f18990ecf954f6e027",
-        "title": "Commodo qui incididunt",
+        "title": "Commodo qui incididunt 123",
         "text": "Commodo qui incididunt ex ut ea nulla et eu aliquip duis. Aute deserunt excepteur ullamco fugiat sunt aliquip exercitation do sint incididunt. Amet consectetur sint irure reprehenderit fugiat amet mollit. In commodo mollit ullamco cillum pariatur eiusmod cillum aute mollit. Culpa non sint eiusmod ad dolor velit dolore voluptate do adipisicing. Cupidatat sint est magna officia qui magna eu elit qui excepteur fugiat duis ex labore.\n\nAliquip veniam ad reprehenderit mollit exercitation id enim ut exercitation. Esse irure ipsum minim laborum reprehenderit irure ut. Tempor excepteur nisi nulla nostrud amet id cillum. Sint velit sint officia aliqua sint quis deserunt.\n\nAliquip dolor cillum deserunt enim nulla dolor amet irure cupidatat commodo laboris id aliqua. Labore aliqua adipisicing Lorem id adipisicing. Ad cupidatat et do anim ex commodo elit magna ad consequat. Nostrud sit eu laborum ut consequat fugiat aute culpa. Lorem tempor quis sunt ad consequat excepteur est. Enim voluptate cillum Lorem ex fugiat ea qui. Irure aute magna dolore eiusmod minim non ad anim dolore sint et.",
         "comments": [0,1,2,3,4]
     },
